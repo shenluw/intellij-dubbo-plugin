c13d668a6826b5694373135c50b1f4c0a533f554
luws <luw1683@163.com>
2019-09-28 22:45:34 +0800

    添加ui布局

---
+++ src/main/java/top/shenluw/plugin/dubbo/ui/DubboWindowForm.java
@@ -0,0 +1,35 @@
+package top.shenluw.plugin.dubbo.ui;
+
+import com.intellij.openapi.ui.SimpleToolWindowPanel;
+
+import javax.swing.*;
+
+/**
+ * @author Shenluw
+ * created：2019/9/28 12:53
+ */
+public class DubboWindowForm extends SimpleToolWindowPanel {
+
+    protected JPanel root;
+    protected JComboBox registryComboBox;
+    protected JButton connectBtn;
+    protected JComboBox applicationComboBox;
+    protected JComboBox serviceComboBox;
+    protected JComboBox methodComboBox;
+    protected JButton execBtn;
+    protected JButton concurrentExecBtn;
+    protected JComboBox paramsEditorTypeSelect;
+    protected JComboBox responseTypeSelect;
+    protected JButton paramsEditorOpenBtn;
+    protected JButton responseOpenBtn;
+    protected JEditorPane paramsEditor;
+    protected JTextPane responseTextPane;
+    protected JComboBox threadGroupCountComboBox;
+    protected JComboBox concurrentCountComboBox;
+
+    public DubboWindowForm() {
+        super(true, true);
+        setContent(root);
+    }
+
+}
